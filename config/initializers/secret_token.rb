8de6b49fd609ccaed02503afabd014a73f8a1890
Robin Miller & Ben Birdsall <student@epicodus.com>
2014-04-21 08:50:09 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TexterApp::Application.config.secret_key_base = 'd049060188fcea988d103c18c693f2cbed309c8d1c1f007ea8ccc37f8877dfab5e0df2feef4528e5902ac331ad627d3d31d69fea1fa0c0ccd619cd3069c05125'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TexterApp::Application.config.session_store :cookie_store, key: '_texter_app_session'
