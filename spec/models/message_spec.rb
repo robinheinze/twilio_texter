90974dcae4f4adafa98498a5ac6ab158ecac9049
Robin Miller & Ben Birdsall <student@epicodus.com>
2014-04-21 10:10:56 -0700

    add a test for number formatting

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,4 +4,9 @@ describe Message do
   it { should validate_presence_of :to }
   it { should validate_presence_of :from }
   it { should validate_presence_of :body }
+
+  it 'should be able to format its phone numbers' do
+    test_message = Message.new(:to => '1111111111', :from => '2222222222', :body => "hello")
+    test_message.formatted_number(test_message.to).should eq '(111) 111-1111'
+  end
 end
