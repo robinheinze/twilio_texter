61ed49105b78abe9b121af75ef6b7fee64679f8b
Ben Birdsall & robin miller <student@epicodus.com>
2014-04-22 10:09:52 -0700

    initial commit of Tuesday's attempt at nested_attribute-based multiple text recipients.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,17 +10,29 @@ class MessagesController < ApplicationController
   def new
     @message = Message.new
     @contacts = Contact.order(:name)
-    @recipient = Recipient.new
   end
 
   def create
     @message = Message.new(message_params)
     if @message.save
-      flash[:notice] = "Your message was sent!"
+      @message.recipients.each do |recipient|
+        if recipient.save
+          flash[:notice] = "Your message to #{recipient.contact.name} was sent!"
+        elsif recipient.errors.any?
+          flash[:alert] = "The following errors occurred:"
+          recipient.errors.full_messages.each do |error_message|
+            flash[:alert] = error_message
+          end
+        end
+      end
       redirect_to messages_path
     else
       render 'new'
     end
+  end
+
+  def get_recipients
+    render :recipient_form, :layout => false
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 TexterApp::Application.routes.draw do
-  resources :messages
+  resources :messages, :except => [:new, :create]
+
+  resources :messages, :only => [:new, :create] do
+    collection do.
+      get :get_recipients
+    end
+  end
+
   resources :recipients, only: :new
   resources :contacts
   root :to => 'messages#index'
